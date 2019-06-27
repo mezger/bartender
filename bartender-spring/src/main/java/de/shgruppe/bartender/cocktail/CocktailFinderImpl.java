7a37cf2b8f9f9098c22ac83861d721024b2cafc7
Stefan Unbehauen <stefan.unbehauen@shks.de>
2019-06-27 11:36:30 +0200

    Cocktail-Model definiert

---
+++ bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
@@ -0,0 +1,20 @@
+package de.shgruppe.bartender.cocktail;
+
+import java.util.List;
+
+import de.shgruppe.bartender.model.Cocktail;
+import de.shgruppe.bartender.model.Ingredient;
+
+public class CocktailFinderImpl implements CocktailFinder
+{
+	// private static Logger log = LoggerFactory.getLogger(CocktailFinderImpl.class);
+
+	@Override
+	public Cocktail getCocktailForIngredients(List<Ingredient> ingredients, boolean noAlcohol)
+	{
+
+		return null;
+	}
+
+
+}
--- bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
@@ -1,6 +1,15 @@
 package de.shgruppe.bartender.model;
 
+import java.util.List;
+
 public class Cocktail
 {
 	// private static Logger log = LoggerFactory.getLogger(Cocktail.class);
+
+	String id;
+	String name;
+	String zubereitung;
+	String image;
+	boolean alkoholisch;
+	List<Ingredient> listIngredients;
 }
