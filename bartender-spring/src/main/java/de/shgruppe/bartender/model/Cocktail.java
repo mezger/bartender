07f3630d3c6548852cdb6c730edd713bf8630f9a
Tim Hägele <tim.haegele@shks.de>
2019-06-27 13:06:52 +0200

    Merge cocktail

--- bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
@@ -12,8 +12,6 @@ public class Cocktail
 	private boolean alkoholisch;
 	private List<String> listIngredients;
 
-<<<<<<< Upstream, based on branch 'master' of https://github.com/mezger/bartender.git
-
 	public Cocktail()
 	{}
 
@@ -100,81 +98,5 @@ public class Cocktail
 	{
 		this.listIngredients = listIngredients;
 	}
-=======
-	private String id;
-	private String name;
-	private String zubereitung;
-	private String image;
-	private boolean alkoholisch;
-	private List<Ingredient> listIngredients;
-
-	public Cocktail()
-	{
-
-	}
-
-	public Cocktail(String id, String name, String zubereitung, String image, boolean alkoholisch, List<Ingredient> listIngredients)
-	{
-		super();
-		this.id = id;
-		this.name = name;
-		this.zubereitung = zubereitung;
-		this.image = image;
-		this.alkoholisch = alkoholisch;
-		this.listIngredients = listIngredients;
-	}
-
-	public String getId()
-	{
-		return id;
-	}
-	public void setId(String id)
-	{
-		this.id = id;
-	}
-	public String getName()
-	{
-		return name;
-	}
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public String getZubereitung()
-	{
-		return zubereitung;
-	}
-	public void setZubereitung(String zubereitung)
-	{
-		this.zubereitung = zubereitung;
-	}
-	public String getImage()
-	{
-		return image;
-	}
-	public void setImage(String image)
-	{
-		this.image = image;
-	}
-	public boolean isAlkoholisch()
-	{
-		return alkoholisch;
-	}
-	public void setAlkoholisch(boolean alkoholisch)
-	{
-		this.alkoholisch = alkoholisch;
-	}
-	public List<Ingredient> getListIngredients()
-	{
-		return listIngredients;
-	}
-	public void setListIngredients(List<Ingredient> listIngredients)
-	{
-		this.listIngredients = listIngredients;
-	}
-
-
-
->>>>>>> d33dac0 Merge changes
 
 }
