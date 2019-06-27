11e34f2eb79b794c50a846b8e4c715c7b13fb2b1
Stefan Unbehauen <stefan.unbehauen@shks.de>
2019-06-27 12:36:30 +0200

    Cocktail-Model angepasst CocktailFinderImpl mit Inhalt gefüllt

--- bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
@@ -1,6 +1,11 @@
 package de.shgruppe.bartender.cocktail;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.client.RestTemplate;
 
 import de.shgruppe.bartender.model.Cocktail;
 import de.shgruppe.bartender.model.Ingredient;
@@ -9,12 +14,34 @@ public class CocktailFinderImpl implements CocktailFinder
 {
 	// private static Logger log = LoggerFactory.getLogger(CocktailFinderImpl.class);
 
+	String cocktailsByIngredientsURL	= "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=";
+	String cocktailsByCoctailIDURL		= "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=";
+
 	@Override
 	public Cocktail getCocktailForIngredients(List<Ingredient> ingredients, boolean noAlcohol)
 	{
+		JSONObject response = null;
+		if( ingredients.size() > 0)
+		{
+			RestTemplate restTemplate = new RestTemplate();
+			response = restTemplate.getForObject(cocktailsByIngredientsURL + ingredients.get(0), JSONObject.class);
 
-		return null;
+			List<String> drinksList = new ArrayList<String>();
+			JSONArray array = response.getJSONArray("drinks");
+			for(int i = 0 ; i < array.length() ; i++)
+			{
+				drinksList.add(array.getJSONObject(i).getString("idDrink"));
+			}
+		}
+
+		return convertResponseToCocktail(response);
 	}
 
+	private Cocktail convertResponseToCocktail(JSONObject response)
+	{
+		Cocktail cocktail = new Cocktail();
 
+
+		return cocktail;
+	}
 }
--- bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/model/Cocktail.java
@@ -6,10 +6,79 @@ public class Cocktail
 {
 	// private static Logger log = LoggerFactory.getLogger(Cocktail.class);
 
-	String id;
-	String name;
-	String zubereitung;
-	String image;
-	boolean alkoholisch;
-	List<Ingredient> listIngredients;
+	private String id;
+	private String name;
+	private String zubereitung;
+	private String image;
+	private boolean alkoholisch;
+	private List<Ingredient> listIngredients;
+
+	public Cocktail()
+	{
+
+	}
+
+	public Cocktail(String id, String name, String zubereitung, String image, boolean alkoholisch, List<Ingredient> listIngredients)
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.zubereitung = zubereitung;
+		this.image = image;
+		this.alkoholisch = alkoholisch;
+		this.listIngredients = listIngredients;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getZubereitung()
+	{
+		return zubereitung;
+	}
+	public void setZubereitung(String zubereitung)
+	{
+		this.zubereitung = zubereitung;
+	}
+	public String getImage()
+	{
+		return image;
+	}
+	public void setImage(String image)
+	{
+		this.image = image;
+	}
+	public boolean isAlkoholisch()
+	{
+		return alkoholisch;
+	}
+	public void setAlkoholisch(boolean alkoholisch)
+	{
+		this.alkoholisch = alkoholisch;
+	}
+	public List<Ingredient> getListIngredients()
+	{
+		return listIngredients;
+	}
+	public void setListIngredients(List<Ingredient> listIngredients)
+	{
+		this.listIngredients = listIngredients;
+	}
+
+
+
+
 }
