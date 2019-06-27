5ada9a0ce1e2a88765347a30ec217e9827cf2741
Daniel Glasbrenner <>
2019-06-27 14:08:54 +0200

    AWS dependencies + WIP RekognitionServiceImpl

--- bartender-spring/src/main/java/de/shgruppe/bartender/model/Emotion.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/model/Emotion.java
@@ -2,5 +2,17 @@ package de.shgruppe.bartender.model;
 
 public enum Emotion
 {
-  HAPPY, SURPRISED, DISGUSTED, SAD, CALM, ANGRY, CONFUSED
+  HAPPY ("Happy"),
+  SURPRISED ("Surprised"),
+  DISGUSTED ("Disgusted"),
+  SAD ("Sad"),
+  CALM ("Calm"),
+  ANGRY ("Angry"),
+  CONFUSED ("Confused");
+
+  private String value;
+
+  private Emotion(String value) {
+    this.value = value;
+  }
 }
