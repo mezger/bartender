618e6248c77dc5d64134aed5a6ef41cf03ff25c0
Stefan Unbehauen <stefan.unbehauen@shks.de>
2019-06-27 15:58:47 +0200

    Es wird nun das Bool miteinbezogen, ob alkoholische Drinks oder nicht

--- bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
@@ -1,8 +1,8 @@
 package de.shgruppe.bartender.cocktail;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,34 +17,82 @@ public class CocktailFinderImpl implements CocktailFinder
 
 	String cocktailsByIngredientsURL	= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
 	String cocktailsByCoctailIDURL		= "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
-	String cocktailsAlcoholic			= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
-	String cocktailsNonAlcoholic		= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
+	String cocktailsAlcOrNoAlcoholicURL	= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=";
 
+
+	/*ToDo: postProcessAfterInitialization
+	JSONObject responseCocktailsAlcoholic;
+	JSONObject responseCocktailsNonAlcoholic;
+	JSONObject responseCocktailsByIngredient;
+
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		responseCocktailsAlcoholic		= restTemplate.getForObject(cocktailsAlcOrNoAlcoholicURL + "Non_Alcoholic", JSONObject.class);
+		responseCocktailsNonAlcoholic	= restTemplate.getForObject(cocktailsAlcOrNoAlcoholicURL + "Alcoholic",		JSONObject.class);
+
+		return bean;
+	}
+	*/
 
 	@Override
 	public Cocktail getCocktailForIngredients(List<Ingredient> ingredients, boolean noAlcohol)
 	{
+		// 1.) Hole mir eine Liste an alkoholischen/nicht alkoholischen Drinks
+		String urlDrinkList = cocktailsAlcOrNoAlcoholicURL;
+		if(noAlcohol)
+		{
+			urlDrinkList += "Non_Alcoholic";
+		}
+		else
+		{
+			urlDrinkList += "Alcoholic";
+		}
+
+		RestTemplate restTemplate = new RestTemplate();
+		JSONObject  responseCocktailsAlcOrNoAlc = restTemplate.getForObject(urlDrinkList, JSONObject.class);
+
+		List<String> listDrinksAlcOrNoAlc = new ArrayList<String>();
+		JSONArray arrayDrinksAlcOrNocAlc = responseCocktailsAlcOrNoAlc.getJSONArray("drinks");
+		for(int i = 0 ; i < arrayDrinksAlcOrNocAlc.length() ; i++)
+		{
+			listDrinksAlcOrNoAlc.add(arrayDrinksAlcOrNocAlc.getJSONObject(i).getString("idDrink"));
+		}
+
+		// 2.) Hole mir eine Liste von DrinkIds mit der übergebenen Zutat
 		JSONObject responseCocktailById = null;
+		List<String> listDrinksByIngredient = new ArrayList<String>();
 		if( ingredients.size() > 0)
 		{
-			// 1.) Hole mir eine Liste von DrinkIds mit dieser Zutat
-			RestTemplate restTemplate = new RestTemplate();
-			JSONObject  responsCocktailByIngrdients = restTemplate.getForObject(cocktailsByIngredientsURL + ingredients.get(0).getShortName(), JSONObject.class);
-
-			List<String> listDrinks = new ArrayList<String>();
-			JSONArray array = responsCocktailByIngrdients.getJSONArray("drinks");
-			for(int i = 0 ; i < array.length() ; i++)
+			JSONObject responseCocktailsByIngredients = restTemplate.getForObject(cocktailsByIngredientsURL + ingredients.get(0).getShortName(), JSONObject.class);
+			JSONArray arrayDrinksByIngridient = responseCocktailsByIngredients.getJSONArray("drinks");
+			for(int i = 0 ; i < arrayDrinksByIngridient.length() ; i++)
 			{
-				listDrinks.add(array.getJSONObject(i).getString("idDrink"));
+				listDrinksByIngredient.add(arrayDrinksByIngridient.getJSONObject(i).getString("idDrink"));
+			}
+		}
+
+		// 3.) Hole mir den Drink, welcher in beiden Listen vertreten ist
+		if(    listDrinksAlcOrNoAlc.size() > 0
+			&& listDrinksByIngredient.size() > 0)
+		{
+			// Beide Listen werden durchgemischt, um einen Random Effekt zu erzielen
+			Collections.shuffle(listDrinksAlcOrNoAlc);
+			Collections.shuffle(listDrinksByIngredient);
+			String drinkId = "";
+
+			for(int i= 0; i < listDrinksByIngredient.size(); i++)
+			{
+				if( listDrinksAlcOrNoAlc.contains(listDrinksByIngredient.get(i)) )
+				{
+					drinkId = listDrinksByIngredient.get(i);
+					break;
+				}
 			}
 
-			// 2.) Hole mir eine DrinkId aus der Liste
-			if( listDrinks.size() > 0 )
+			if( !drinkId.isEmpty())
 			{
-				// Falls sich mehrere DrinkIds in der Liste befinden, wird sich per Zufall eine ID geholt
-				Random random = new Random();
-				String randomDrinkID = listDrinks.get(random.nextInt(listDrinks.size()));
-				responseCocktailById = restTemplate.getForObject(cocktailsByCoctailIDURL + randomDrinkID, JSONObject.class);
+				responseCocktailById = restTemplate.getForObject(cocktailsByCoctailIDURL + drinkId, JSONObject.class);
 			}
 		}
 
@@ -54,29 +102,33 @@ public class CocktailFinderImpl implements CocktailFinder
 	private Cocktail convertResponseToCocktail(JSONObject response)
 	{
 		Cocktail cocktail = new Cocktail();
-		JSONObject drink = response.getJSONObject("drinks");
 
-		cocktail.setId			(drink.getString("idDrink"));
-		cocktail.setName		(drink.getString("strDrink"));
-		cocktail.setZubereitung	(drink.getString("strInstructions"));
-		cocktail.setImage		(drink.getString("strDrinkThumb"));
-
-		String alkoholisch			= drink.getString("strAlcoholic");
-		boolean isDrinkAlcoholic	= alkoholisch.equals("Alcoholic");
-		cocktail.setAlkoholisch(isDrinkAlcoholic);
-
-		// Es sind maximal 15 Ingredient im JSON-Object vorhanden
-		List<String> listIngredients = new ArrayList<String>();
-		for(int i = 1; i <= 15; i++)
+		if( response != null )
 		{
-			String ingredient = drink.getString("strIngredient" + i);
-			if( !ingredient.isEmpty())
-			{
-				listIngredients.add(ingredient);
-			}
-		}
+			JSONObject drink = response.getJSONObject("drinks");
 
-		cocktail.setListIngredients(listIngredients);
+			cocktail.setId			(drink.getString("idDrink"));
+			cocktail.setName		(drink.getString("strDrink"));
+			cocktail.setZubereitung	(drink.getString("strInstructions"));
+			cocktail.setImage		(drink.getString("strDrinkThumb"));
+
+			String alkoholisch			= drink.getString("strAlcoholic");
+			boolean isDrinkAlcoholic	= alkoholisch.equals("Alcoholic");
+			cocktail.setAlkoholisch(isDrinkAlcoholic);
+
+			// Es sind maximal 15 Ingredient im JSON-Object vorhanden
+			List<String> listIngredients = new ArrayList<String>();
+			for(int i = 1; i <= 15; i++)
+			{
+				String ingredient = drink.getString("strIngredient" + i);
+				if( !ingredient.isEmpty())
+				{
+					listIngredients.add(ingredient);
+				}
+			}
+
+			cocktail.setListIngredients(listIngredients);
+		}
 
 		return cocktail;
 	}
