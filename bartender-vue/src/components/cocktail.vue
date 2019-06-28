47b2fefa9324674797180047c6f56bd7a905439c
Stefan Bohn <stefan@bohni.net>
2019-06-28 15:42:11 +0200

    Zutaten und Zubereitung anzeigen

--- bartender-vue/src/components/cocktail.vue
+++ bartender-vue/src/components/cocktail.vue
@@ -3,9 +3,20 @@
     <p class="result">Nachdem Du gerade<br><strong>{{ stimmung }}</strong><br>bist,
     </p>
     <p class="result">
-      empfehlen wir Dir einen<br>
-      <img v-bind:src="cocktail.image" v-bind:title="cocktail.name" v-bind:alt="cocktail.name" width="50%"/><br>
+      empfehlen wir Dir einen
+    </p>
+    <p>
+      <img v-bind:src="cocktail.image" v-bind:title="cocktail.name" v-bind:alt="cocktail.name" width="50%"/>
+    </p>
+    <p class="result">
       <strong>{{ cocktail.name }}</strong>
+    </p>
+    <p class="result" style="font-weight: bold; text-align:">Ingredients</p>
+    <ul class="result" v-for="ingredient in cocktail.listIngredients">
+      <li>{{ingredient}}</li>
+    </ul>
+    <p class="result">
+      {{cocktail.zubereitung}}
     </p>
   </div>
 </template>
@@ -20,11 +31,12 @@ export default {
   }
 }
 </script>
+
 <style scoped>
-p.result {
+.result {
   font-size: 1.5em;
   }
-p.result strong {
+.result strong {
   font-size: 2em;
 }
 </style>
