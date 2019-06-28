18cc86bc78ba1ae940b93f10898b664e65215d96
FranzBuehler <franz.buehler@shks.de>
2019-06-28 09:33:03 +0200

    $ entfernt

--- bartender-vue/src/main.js
+++ bartender-vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
     
-Vue.use(VueAxios, axios, $)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
