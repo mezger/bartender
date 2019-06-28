bd7414c2146195ea9db0e531b60fbc613188ab41
Stefan Unbehauen <stefan.unbehauen@shks.de>
2019-06-28 12:38:47 +0200

    Liste der alkoholischen Drinks und nicht alkoholischen Drinks gefixed

--- bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
+++ bartender-spring/src/main/java/de/shgruppe/bartender/cocktail/CocktailFinderImpl.java
@@ -12,6 +12,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,27 +24,43 @@ import de.shgruppe.bartender.model.Ingredient;
 @Lazy
 public class CocktailFinderImpl implements CocktailFinder
 {
+	@Autowired
+	RestTemplate restTemplate;
+
 	private static Logger log = LoggerFactory.getLogger(CocktailFinderImpl.class);
 
 	String cocktailsByIngredientsURL	= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
 	String cocktailsByCoctailIDURL		= "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 	String cocktailsAlcOrNoAlcoholicURL	= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=";
 
-	JSONObject jsonCocktailsAlcoholic;
-	JSONObject jsonCocktailsNonAlcoholic;
+	List<String> listCocktailsAlc;
+	List<String> listCocktailsNoAlc;
 
 	@PostConstruct
 	public void initCocktailFinder()
 	{
 		// Listen der alkoholischen / nicht alkoholischen Cocktails wird zum Start geladen und sind die Anwendung über dann verfügbar
-		RestTemplate restTemplate				= new RestTemplate();
 		String responseCocktailsAlcoholic		= restTemplate.getForObject(cocktailsAlcOrNoAlcoholicURL + "Alcoholic", 		String.class);
 		String responseCocktailsNonAlcoholic	= restTemplate.getForObject(cocktailsAlcOrNoAlcoholicURL + "Non_Alcoholic",		String.class);
 
 		try
 		{
-			jsonCocktailsAlcoholic		= new JSONObject(responseCocktailsAlcoholic);
-			jsonCocktailsNonAlcoholic	= new JSONObject(responseCocktailsNonAlcoholic);
+			JSONObject jsonCocktailsAlcoholic = new JSONObject(responseCocktailsAlcoholic);
+			listCocktailsAlc = new ArrayList<String>();
+			JSONArray arrayCocktailsFilteredByAlc = jsonCocktailsAlcoholic.getJSONArray("drinks");
+			for(int i = 0 ; i < arrayCocktailsFilteredByAlc.length() ; i++)
+			{
+				listCocktailsAlc.add(arrayCocktailsFilteredByAlc.getJSONObject(i).getString("idDrink"));
+			}
+
+			JSONObject jsonCocktailsNonAlcoholic	= new JSONObject(responseCocktailsNonAlcoholic);
+
+			listCocktailsNoAlc = new ArrayList<String>();
+			JSONArray arrayCocktailsFilteredByNoAlc = jsonCocktailsNonAlcoholic.getJSONArray("drinks");
+			for(int i = 0 ; i < arrayCocktailsFilteredByNoAlc.length() ; i++)
+			{
+				listCocktailsNoAlc.add(arrayCocktailsFilteredByNoAlc.getJSONObject(i).getString("idDrink"));
+			}
 
 			log.info("Alkoholische und nicht-alkoholische wurden geladen.");
 		}
@@ -58,27 +75,21 @@ public class CocktailFinderImpl implements CocktailFinder
 	public Cocktail getCocktailForIngredients(List<Ingredient> ingredients, boolean noAlcohol)
 	{
 		// 1.) Setze die Cocktail-Liste, gefiltert nach alkoholisch oder nicht alkoholischen Drinks
-		JSONObject  jsonCocktailsFilteredByAlcOrNoAlc;
+		List<String> listCocktails;
 		if(noAlcohol)
 		{
-			jsonCocktailsFilteredByAlcOrNoAlc = jsonCocktailsNonAlcoholic;
+			listCocktails = listCocktailsNoAlc;
 		}
 		else
 		{
-			jsonCocktailsFilteredByAlcOrNoAlc = jsonCocktailsAlcoholic;
-		}
-
-		// Speichere die Drink-IDs in der Liste
-		List<String> listCocktailsFilteredByAlcNoAlc = new ArrayList<String>();
-		JSONArray arrayCocktailsFilteredByAlcNoAlc = jsonCocktailsFilteredByAlcOrNoAlc.getJSONArray("drinks");
-		for(int i = 0 ; i < arrayCocktailsFilteredByAlcNoAlc.length() ; i++)
-		{
-			listCocktailsFilteredByAlcNoAlc.add(arrayCocktailsFilteredByAlcNoAlc.getJSONObject(i).getString("idDrink"));
+			// Falls der User volljaehrig ist, bekommt er entweder alkoholische oder nicht
+			// alkoholische Cocktails
+			listCocktails = listCocktailsNoAlc;
+			listCocktails.addAll(listCocktailsNoAlc);
 		}
 
 		// 2.) Hole mir eine Liste von DrinkIds mit der übergebenen Zutat
 		JSONObject jsonCocktailById				= null;
-		RestTemplate restTemplate				= new RestTemplate();
 		List<String> listCocktailsByIngredient	= new ArrayList<String>();
 		if( ingredients.size() > 0)
 		{
@@ -93,11 +104,11 @@ public class CocktailFinderImpl implements CocktailFinder
 		}
 
 		// 3.) Es wird ein Cocktail geholt, welcher in beiden Listen vertreten ist
-		if(    listCocktailsFilteredByAlcNoAlc.size() > 0
+		if(    listCocktails.size() > 0
 			&& listCocktailsByIngredient.size() > 0)
 		{
 			// Beide Listen werden durchgemischt, um einen Random Effekt zu erzielen
-			Collections.shuffle(listCocktailsFilteredByAlcNoAlc);
+			Collections.shuffle(listCocktails);
 			Collections.shuffle(listCocktailsByIngredient);
 			String drinkId = "";
 
@@ -105,7 +116,7 @@ public class CocktailFinderImpl implements CocktailFinder
 
 			for(int i= 0; i < listCocktailsByIngredient.size(); i++)
 			{
-				if( listCocktailsFilteredByAlcNoAlc.contains(listCocktailsByIngredient.get(i)) )
+				if( listCocktails.contains(listCocktailsByIngredient.get(i)) )
 				{
 					drinkId = listCocktailsByIngredient.get(i);
 					foundDrink = true;
@@ -126,7 +137,7 @@ public class CocktailFinderImpl implements CocktailFinder
 				log.warn("Es wurde kein konkreter Drink anhand den Parametern gefunden");
 				log.info("Es wird ein Random Drink geladen...");
 				Random rand = new Random();
-				String randomDrinkID = listCocktailsFilteredByAlcNoAlc.get(rand.nextInt(listCocktailsFilteredByAlcNoAlc.size()));
+				String randomDrinkID = listCocktails.get(rand.nextInt(listCocktails.size()));
 				jsonCocktailById = getDrinkById(randomDrinkID);
 
 			}
@@ -186,7 +197,6 @@ public class CocktailFinderImpl implements CocktailFinder
 	private JSONObject getDrinkById( String drinkID )
 	{
 		JSONObject jsonDrink = new JSONObject();
-		RestTemplate restTemplate = new RestTemplate();
 		String responseRandomCocktail = restTemplate.getForObject(cocktailsByCoctailIDURL + drinkID, String.class);
 		try
 		{
